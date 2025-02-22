a5e23485e4fa16d63dcc8e3df1701bfe7d5e1fe8
Maisam Anjum <maisam@Maisams-MacBook-Pro.local>
2025-02-22 17:59:51 -0500

    changes scripts to streamline deployment

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,23 @@ set -e
 
 echo "Starting the build process..."
 
-# Step 1: Run the build
+# Run the build
 npm run build
 
 echo "Build successful! Proceeding with branch update..."
 
-# Step 2: Switch to the main branch
+# Switch to the main branch
 git checkout main
 
-# Step 3: Reset main to match Dev
+# Reset main to match Dev
 git reset --hard Dev
 
-# Step 4: Push changes to GitHub
+# Push changes to GitHub
 git push --force origin main
 
 echo "Dev branch has been successfully copied to Main!"
+
+# deploy
+npm run deploy
+
+echo "Deployed from main"
